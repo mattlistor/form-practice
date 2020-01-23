65ab0390220dce4ebc68b408c70dfdd42b694a52
mattlistor <mattlistor@gmail.com>
2020-01-22 23:12:32 -0500

    practicing forms

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ class App extends React.Component  {
   state = {
     score: "",
     name: "",
-    list: []
+    list: [{name: "Matthew", score: "1234"}]
   }
 
+  // {name: "Name", score: "Score"}
+
   handleChange = (event) => {
-    this.setState({[event.target.name]: event.target.value});
+    if (event.target.name === "score"){
+      if (event.target.value.toString().length <= 4){
+
+        this.setState({score: event.target.value});
+      }
+    }
+    if (event.target.name === "name"){
+        this.setState({name: event.target.value});
+    }
   }
 
   handleSubmit = (event) => {
@@ -22,24 +32,26 @@ class App extends React.Component  {
       this.setState({
         score: "",
         name: "",
-        list: [newUser, ...this.state.list]
+        list: [...this.state.list, newUser]
       });
     }
     
   }
 
   render(){
-    return (    
-      <div className="App">   
-  
+    return (  
+
+      <div className="App">  
         <form className="form" onSubmit={(e) => this.handleSubmit(e, this.state)}>
+          <div className="header">High Score List</div> 
           <input className="textField" id="inputField" type="text" name="name" placeholder="Name..." value={this.state.name} onChange={(e) => this.handleChange(e)}/>
-          <input className="textField" id="inputField" type="number" name="score" placeholder="Score..." value={this.state.score} onChange={(e) => this.handleChange(e)}/>
+          <input className="scoreField" id="inputField" type="number" name="score" maxLength="4" placeholder="Score..." value={this.state.score} onChange={(e) => this.handleChange(e)}/>
           <br></br>
           <input className="submit" type="submit" value="Submit" />
         </form>
         <List list={this.state.list}/>
       </div>
+
     );
   } 
 }
--- src/List.js
+++ src/List.js
@@ -3,11 +3,21 @@ import './App.css';
 
 class List extends React.Component  {
   
+
+  //Sorts by high score
+
   render(){
-    let list = this.props.list.map(userObj => <li>{userObj.name}</li>)
+    let list = this.props.list.map(userObj => <div className="listItem"><div className="name">{userObj.name.substring(0,7)}</div><div className="score">{userObj.score}</div></div>)
     return(
       <div className="List">
+
+        <div className="listHeader">
+          <div className="name">Username</div>
+          <div className="score">Score</div>
+        </div>
+
         {list}
+        
       </div>
     );
   } 
